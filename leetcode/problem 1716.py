ee3fd251e4b252d1a628618c926747e5a6d1d396
shirshadip <artistavra@gmail.com>
2025-10-26 00:25:20 +0530

    solved

--- leetcode/problem 1716.py
+++ leetcode/problem 1716.py
@@ -2,8 +2,11 @@ class Solution:
     def totalMoney(self, n: int) -> int:
         week = n //7#number of complete weeks 
         rem = n %7 #number of remaining days
-        print (week)
-        print (rem)
+        
+        total_full_weeks = 28 * week + 7 * week * (week - 1) // 2
+        total_remaining = rem * (week + 1) + rem * (rem - 1) // 2
+        
+        return total_full_weeks + total_remaining
 a = Solution()
 print (a.totalMoney(4))#10
 print (a.totalMoney(10))#37
